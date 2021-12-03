b641db25b9af1dc8ecb0ac80549e30529d13dacd
adamvinicius <adamvinicius@gmail.com>
2021-12-02 21:28:29 -0300

    aula 08 introducao restassured

---
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -0,0 +1,39 @@
+package plataformaFilmes;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlataformaFilmesTest {
+
+    @Test
+    public void validarLogin(){
+        RestAssured.baseURI = "http://localhost:8080/";
+
+        String json = "{" +
+                "    \"email\": \"aluno@email.com\"," +
+                "    \"senha\": \"123456\"" +
+                "}";
+
+        Response response = post(json, ContentType.JSON, "auth");
+
+        assertEquals(200, response.statusCode());
+        String token = response.jsonPath().get("token");
+
+
+    }
+
+    public Response post(Object json, ContentType contentType, String endpoint){
+        return RestAssured.given()
+                .relaxedHTTPSValidation()
+                .contentType(contentType)
+                .body(json)
+                .when()
+                .post(endpoint)
+                .thenReturn();
+
+    }
+}
