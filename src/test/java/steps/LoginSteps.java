29f4fa03f0ea6e8940deb3c51195b8baaeded478
adamvinicius <34081980+adamvinicius@users.noreply.github.com>
2022-01-23 21:18:10 -0300

    teste Jenkins

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -12,12 +12,12 @@ import java.util.Map;
 
 public class LoginSteps {
 
-    String url = "http://localhost:8080/";
+    
 
     @Dado("que tenha um payload valido da API de Login")
     public void queTenhaUmPayloadValidoDaAPIDeLogin() {
         LoginMap.initLogin();
-        RestUtils.setBaseURI(url);
+        
     }
     @Quando("envio uma requisicao do tipo POST de Login")
     public void envioUmaRequisicaoDoTipoPOSTDeLogin() {
@@ -32,7 +32,7 @@ public class LoginSteps {
     @Dado("que tenha um payload da API de Login com as seguintes informacoes")
     public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(Map<String, String> map) {
         LoginMap.initLogin();
-        RestUtils.setBaseURI(url);
+        
         LoginMap.getLogin().putAll(map);
     }
 
